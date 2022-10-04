6a8bf1dfa0983b3998eab3ce4aae8efb5be832e3
logalex8369 <112826959+logalex8369@users.noreply.github.com>
2022-10-04 18:25:31 +0000

    had daddy help because my variables weren't working

--- HalloweenScramble/game.js
+++ HalloweenScramble/game.js
@@ -11,7 +11,7 @@
    - Sometimes users accidently add a space to the end of their words so while they mean to submit the string "pumpkin" they actually submit "pumpkin " -- not the space at the end.
    - Go to the online docs, under strings and look up the .trim() function. 
 
-  DONE I THINK TODO MAYBE 4: Right now you have to reload the page each time. That means the game really can't keep score. Alter the code so that when the player successfully unscrambles a word the game will call the nextWord function.  
+  DONE 4: Right now you have to reload the page each time. That means the game really can't keep score. Alter the code so that when the player successfully unscrambles a word the game will call the nextWord function.  
     - Move the variable allWords into the nextWord() function
     - Move the code that picks a random word into the nextWord() function
     - Move the code that writes a scrambled word to screen into the nextWord() function
@@ -29,13 +29,13 @@
     - This is how you SET/GET the HTML of the "unscrambled" div:  document.getElementById("unscrambled").innerHTML="";
     - Think back to class when I showed you how we can add strings together.  That is what you want to do here.  You want to combine the existing string value of the "unscrambled" div with the string value of the unscrambled word. 
     
-  TODO 6: The game should end when the user unscrambles a word incorrectly
+  DONE 6: The game should end when the user unscrambles a word incorrectly
     - Create a new variable to track if the game is over. (what type should that be, number, string, bool?)
     - Initialize that new variable by setting it to false when the page loads
     - You should set that variable to true if the user gives up or fails to unscramble a word. 
     - HINT Look in the CHECK function, there is a conditional that checks IF the word is correct. Now you need to add an ELSE and in that else condition we want to:
 
-  TODO 7:
+  DONE 7:
     - We should give the player some sort of message when the game is over and maybe include how many words they successfully unscrambled. Something like:
       - Game Over! You unscrabled __ words!
 
@@ -44,50 +44,50 @@
 *********************************************************************/
 
 
-// Our master array of Halloween words.  Note how it is declared as a CONST. Thats because we dont want anything changing these values
-const allWords = ["Costumes", "Monster", "Disguise", "Ghost", "Witch", "Pumpkin", "Candle", "Zombie", "Frankenstein", "October", "Scarecrow", "Pirate", "Crow", "Cat", "Broomstick", "Vampire", "Prince", "Princess", "Candy", "Werewolf", "Mask", "Spell", "Goblin", "Ghoul", "Alien", "Mummy", "Spooky", "Creepy", "Slimy", "Fangs", "Blood", "Skeleton", "Graveyard", "Party", "Screaming", "Bats", "Skull", "Wicked", "Scary"];
+var selectedWord = "";
 
-//Select a random word from the "allWords" array and assign it to a variable called "selectedWord"
-let randomIndex = Math.floor(Math.random() * (allWords.length - 1)); //Don't worry about this math, but it will select a random number between 0 and the last index of the allWords array.
-let selectedWord = allWords[randomIndex]; // Use that random index in allWords and set that to a new variable that will hold our selectedWord.
-
-
-//Scramble the selectedWord and display it inside of a div called "scrambledWord"
-document.getElementById("scrambledWord").innerHTML = selectedWord.toLowerCase()
-  .split("") //This will split the word into an array of letters
-  .sort(() => Math.random() - 0.5) //This will randomly sort the array of letters
-  .join(""); // Now we want to join all those array elements back together into a string
-
-
+let countCorrect = 0;
+let gameOver = false;
 
 // This is a placeholder. You will need to move code into this funcation as part of the assignment. HINT -- the code you want to move into this function is above. :)
 function nextWord() {
   // Our master array of Halloween words.  Note how it is declared as a CONST. Thats because we dont want anything changing these values
   const allWords = ["Costumes", "Monster", "Disguise", "Ghost", "Witch", "Pumpkin", "Candle", "Zombie", "Frankenstein", "October", "Scarecrow", "Pirate", "Crow", "Cat", "Broomstick", "Vampire", "Prince", "Princess", "Candy", "Werewolf", "Mask", "Spell", "Goblin", "Ghoul", "Alien", "Mummy", "Spooky", "Creepy", "Slimy", "Fangs", "Blood", "Skeleton", "Graveyard", "Party", "Screaming", "Bats", "Skull", "Wicked", "Scary"];
-  
+
   //Select a random word from the "allWords" array and assign it to a variable called "selectedWord"
   let randomIndex = Math.floor(Math.random() * (allWords.length - 1)); //Don't worry about this math, but it will select a random number between 0 and the last index of the allWords array.
-  let selectedWord = allWords[randomIndex]; // Use that random index in allWords and set that to a new variable that will hold our selectedWord.
-  
+  let currentWord = allWords[randomIndex]; // Use that random index in allWords and set that to a new variable that will hold our selectedWord.
+
   //Scramble the selectedWord and display it inside of a div called "scrambledWord"
-  document.getElementById("scrambledWord").innerHTML = selectedWord.toLowerCase()
+  document.getElementById("scrambledWord").innerHTML = currentWord.toLowerCase()
     .split("") //This will split the word into an array of letters
     .sort(() => Math.random() - 0.5) //This will randomly sort the array of letters
     .join(""); // Now we want to join all those array elements back together into a string
-
+  return currentWord;
 }
 
 // This function is called when the user clicks on the "Check" button.
 function check() {
   if (selectedWord.toLowerCase() === document.getElementById("myWord").value.toLowerCase().trim()) {
     alert("CORRECT!")
-    nextWord();
+    countCorrect++;
+    selectedWord = nextWord();
+  } else {
+    if (countCorrect == 1) {
+      alert("You unscrambled 1 word correctly")
+    } else {
+      alert("You unscrambled " + countCorrect + " words correctly")
+    }
+    let gameOver = true;
+    let countCorrect = 0;
   }
 }
 
 //This function gets called when the user clicks on the "Give Up" button.
 function giveUp() {
   document.getElementById("myWord").value = selectedWord;
+  let gameOver = true;
+  let countCorrect = 0;
 }
 
-nextWord();
+selectedWord = nextWord();
