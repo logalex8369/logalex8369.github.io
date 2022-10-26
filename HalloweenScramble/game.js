74b0ceaafc3233c6192a58160d1ff822d7a3ec2b
logalex8369 <112826959+logalex8369@users.noreply.github.com>
2022-10-26 00:26:27 +0000

    almost done halloween scramble :)

--- HalloweenScramble/game.js
+++ HalloweenScramble/game.js
@@ -1,34 +1,16 @@
 /********************************************************************
   --- Now that we dont have to reload the page to get a new word, we can actually build a game --- 
-
-  TODO 5: 
-    - We need to keep track of successfully unscrambled words.  There's a DIV in the HTML called "unscrambled", we need to populate it with the words when they are unscrambled.
-    - This is how you SET/GET the HTML of the "unscrambled" div:  document.getElementById("unscrambled").innerHTML="";
-    - Think back to class when I showed you how we can add strings together.  That is what you want to do here.  You want to combine the existing string value of the "unscrambled" div with the string value of the unscrambled word. 
-
   TODO 8:
     - The UI is BORING! It needs styling and images
 *********************************************************************/
 
 
 var selectedWord = "";
-
+//TODO: countcorrect variable not globally transported
 let countCorrect = 0;
 let gameOver = false;
 lucky = 0;
 luckyAmount = 0;
-
-//TODO: "Save Changes" Button not working
-const cb = document.getElementById('accept');
-const btn = document.getElementById('btn');
-const cb2 = document.getElementById('accept2');
-
-btn.onclick = () => {
-  alert(cb.value);
-  alert(cb2.value);
-  let correctAlert = cb.value;
-  let correctAlert = cb2.value;
-};
 
 // This is a placeholder. You will need to move code into this funcation as part of the assignment. HINT -- the code you want to move into this function is above. :)
 function nextWord() {
@@ -45,19 +27,21 @@ function nextWord() {
     .sort(() => Math.random() - 0.5) //This will randomly sort the array of letters
     .join(""); // Now we want to join all those array elements back together into a string
   return currentWord;
-  
+
 }
 
-
+//TODO: textbox color not working properly
 // This function is called when the user clicks on the "Check" button.
 function check() {
   if (selectedWord.toLowerCase() === document.getElementById("myWord").value.toLowerCase().trim()) {
-    alert("You have " + countCorrect + " correct!")
-    document.getElementById("unscrambled").innerHTML="";
+    document.getElementById("myWord").style.background = "#00b000"
     countCorrect++;
+    alert("You have " + countCorrect + " correct!")
+    document.getElementById("unscrambled").innerHTML += "<br>" + selectedWord;
     selectedWord = nextWord();
+    document.getElementById("myWord").style.background = "#ffffff"
   } else {
-    //TODO: Not executing
+    document.getElementById("myWord").style.background = "#cf2222"
     if (countCorrect == 1) {
       alert("You unscrambled 1 word correctly")
     } else {
@@ -65,14 +49,23 @@ function check() {
     }
     let gameOver = true;
     let countCorrect = 0;
+    document.getElementById("myWord").style.background = "#ffffff"
+    return gameOver;
   }
 }
 
 //This function gets called when the user clicks on the "Give Up" button.
 function giveUp() {
+  document.getElementById("myWord").style.background = "#cf2222"
   document.getElementById("myWord").value = selectedWord;
+  if (countCorrect == 1) {
+    alert("You unscrambled 1 word correctly")
+  } else {
+    alert("You unscrambled " + countCorrect + " words correctly")
+  }
   let gameOver = true;
   let countCorrect = 0;
+  document.getElementById("myWord").style.background = "#ffffff"
   return countCorrect;
   return gameOver;
 }
